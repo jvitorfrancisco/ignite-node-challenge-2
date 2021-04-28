ebd25971ce3d8102f2e1242450bedc5ce3bd7db6
Joao Vitor de Francisco <jvitorfrancisco.dev@gmail.com>
2021-04-27 21:32:59 -0300

    Finished

--- src/index.js
+++ src/index.js
@@ -10,19 +10,65 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const username = request.headers['username'];
+
+  const user = users.find(u => u.username == username);
+
+  if(!user)
+    return response.status(404);
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  if(!request.user.pro){
+    if(request.user.todos.length >= 10)
+      return response.status(403);
+  }
+
+  next();
 }
 
-function checksTodoExists(request, response, next) {
-  // Complete aqui
+async function checksTodoExists(request, response, next) {
+  const id = request.params.id;
+
+  const username = request.headers['username'];
+
+  const user = users.find(u => u.username == username);
+
+  if(!user)
+    return response.status(404);
+
+  const isIdValid = validate(id);
+
+  if(!isIdValid)
+    return response.status(400);
+
+  const todo = user.todos.find(t => t.id === id);
+
+  if(!todo)
+    return response.status(404);
+
+  request.user = user;
+  request.todo = todo;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const id = request.params.id;
+
+  const user = users.find(u => u.id === id);
+
+  if(!user) {
+    return response.status(404);
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
